f498b0595ec4242bf5b27c547b8a5d7fdd7a0c74
Pavel <artpavel@ukr.net>
2022-04-09 20:06:23 +0300

    add tabs and scroll

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
 'use strict';
 
 // Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = e => {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openModal);
-}
+btnsOpenModal.forEach(item => {
+  item.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', e => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+/*-------- button scrolling ------------*/
+const btnScroollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScroollTo.addEventListener('click', () => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/*-------- page navigation ------------*/
+document.querySelector('.nav__links').addEventListener('click', e => {
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+/*-------- tabbed component (tabs) ------------*/
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.operations__tab');
+  // when click not element
+  if (!clicked) {
+    return;
+  }
+  // first removed the class and then added
+  tabs.forEach(item => item.classList.remove('operations__tab--active'));
+  tabsContent.forEach(item =>
+    item.classList.remove('operations__content--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+  // show content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
